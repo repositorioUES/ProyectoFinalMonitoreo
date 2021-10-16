0eecfe67e3a2beb46dafd5af6be347eb15a61fb2
Christian <kriz10@outlook.com>
2021-10-16 17:46:49 -0600

    Inicializacion de proyecto final

---
+++ src/test/java/ues/edu/sv/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ues.edu.sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
